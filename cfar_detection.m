c16f9d5256742a5f44cc3db1eabeabd7b3d5f27a
Haisun Banh <dal920981@utdallas.edu>
2026-03-29 15:21:28 -0500

    changed cfar detector to used improved method of noise estimate

--- cfar_detection.m
+++ cfar_detection.m
@@ -4,7 +4,7 @@
 %                           rows: kv
 %                           cols: kr
 %               Pfa         Prob of false alarm
-%               window      rectangular window size, ie. [3, 1] (cols, rows)
+%               window      rectangular window size, ie. [3, 1] (rows, col)
 %
 % Outputs:
 %               detects     1's based indices of image of detections
@@ -18,15 +18,32 @@ function detects = cfar_detection(image, Pfa, window)
 
     threshold = prod(window) *(Pfa^(-1/(prod(window))) -1);
     
-    %padded_image = zeros(size(image,1) + window(1)-1, size(iamge,2) + window(2)-1);
     filt = ones(size_window)/(prod(window)-1);
     filt((window(1)+1)/2, (window(2)+1)/2) = 0;
 
-    % FIXME: This is just a standard square law CFAR algorithm.
-    %       How do we make this the "improved" version in the paper
-    %       that has conditioning on if (x1,y1) is a target?
-    U = conv2(image, filt, "same");
+    if (0)
+        U = conv2(image, filt, "same");
 
-    detects = find(threshold*U > image);
+        detects = find(threshold*U > image);
+    else
+        % Improved CA CFAR detector
+        % FIXME: should we be replicating instead of zero padding?
+        %        or maybe, the filter shouldn't sum values in the pad regions.
+        %padded_image = zeros(size(image,1) + window(1)-1, size(image,2) + window(2)-1);
+        r_offset = (window(1)+1)/2 - 1;
+        c_offset = (window(2)+1)/2 - 1;
+        padded_image = padarray(image, [r_offset, c_offset], 'replicate');
+        detects = [];
+
+        for r = 1:size(image,1)
+            for c = 1:size(image,2)
+                U = sum(sum(padded_image(r:r+window(1)-1, c:c+window(2)-1) .* filt));
+                if (image(r,c) > threshold*U)
+                    detects = [detects; r, c];
+                    padded_image(r+r_offset, c+c_offset) = U; % "condB" if pixel is a detect, use it's noise estimate to calculate U for other pixels
+                end
+            end
+        end
+    end
 
 end
